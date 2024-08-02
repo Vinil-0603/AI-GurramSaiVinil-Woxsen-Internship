407d6f180b5ddee781f88fb7a90ee1d5c134d80a
Gurram Sai Vinil <saivineelreddy5@gmail.com>
2024-08-03 03:15:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 SCRAPE_URL = "https://news.metal.com/list/industry/aluminium"
 EMBEDDING_MODEL = "text-embedding-ada-002"
 LLM_MODEL = "gpt-4"  
